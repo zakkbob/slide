bd9a295df3f4ba4eadc28b7993a39779d9fd6476
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-07-25 15:06:25 +0100

    Add debug mode and split up code

---
+++ cmd/bot/bot.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func (a *application) startGame() {
+	_, b, err := a.client.PostMessage(a.channelID, slack.MsgOptionText("test", false))
+	if err != nil {
+		a.logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	upBtnText := slack.NewTextBlockObject("plain_text", ":upvote:", true, false)
+	upBtn := slack.NewButtonBlockElement("", "click_me", upBtnText)
+	leftBtnText := slack.NewTextBlockObject("plain_text", ":leftvote:", true, false)
+	leftBtn := slack.NewButtonBlockElement("", "click_me", leftBtnText)
+	rightBtnText := slack.NewTextBlockObject("plain_text", ":rightvote:", true, false)
+	rightBtn := slack.NewButtonBlockElement("", "click_me", rightBtnText)
+	downBtnText := slack.NewTextBlockObject("plain_text", ":downvote-red:", true, false)
+	downBtn := slack.NewButtonBlockElement("", "click_me", downBtnText)
+
+	arrowBlock := slack.NewActionBlock("", leftBtn, downBtn, upBtn, rightBtn)
+
+	msg := slack.NewBlockMessage(
+		arrowBlock,
+	)
+
+	_, b, err = a.client.PostMessage(a.channelID, slack.MsgOptionBlocks(msg.Blocks.BlockSet...))
+	if err != nil {
+		a.logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	a.logger.Info("Made a post!", "timestamp", b)
+
+}
--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -8,7 +8,15 @@ import (
 	"github.com/slack-go/slack"
 )
 
+type application struct {
+	debug     bool
+	channelID string
+	client    *slack.Client
+	logger    slog.Logger
+}
+
 func main() {
+	debug := flag.Bool("debug", false, "Debug Mode")
 	channelID := flag.String("channel", "", "Channel ID")
 	apikey := flag.String("apikey", "", "API Key")
 	flag.Parse()
@@ -25,34 +33,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	client := slack.New(*apikey, slack.OptionDebug(true))
-	a, b, err := client.PostMessage(*channelID, slack.MsgOptionText("test", false))
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
+	client := slack.New(*apikey, slack.OptionDebug(*debug))
+
+	app := application{
+		debug:     *debug,
+		channelID: *channelID,
+		logger:    *logger,
+		client:    client,
 	}
 
-	upBtnText := slack.NewTextBlockObject("plain_text", ":upvote:", true, false)
-	upBtn := slack.NewButtonBlockElement("", "click_me", upBtnText)
-	leftBtnText := slack.NewTextBlockObject("plain_text", ":leftvote:", true, false)
-	leftBtn := slack.NewButtonBlockElement("", "click_me", leftBtnText)
-	rightBtnText := slack.NewTextBlockObject("plain_text", ":rightvote:", true, false)
-	rightBtn := slack.NewButtonBlockElement("", "click_me", rightBtnText)
-	downBtnText := slack.NewTextBlockObject("plain_text", ":downvote-red:", true, false)
-	downBtn := slack.NewButtonBlockElement("", "click_me", downBtnText)
-
-	arrowBlock := slack.NewActionBlock("", leftBtn, downBtn, upBtn, rightBtn)
-
-	msg := slack.NewBlockMessage(
-		arrowBlock,
-	)
-
-	a, b, err = client.PostMessage(*channelID, slack.MsgOptionBlocks(msg.Blocks.BlockSet...))
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
-	}
-
-	logger.Info("Made a post!", "a", a, "b", b)
+	app.startGame()
 
 }
