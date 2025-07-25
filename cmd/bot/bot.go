2939c501d13fe1dd27f48294b28c8482a6a3e771
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-07-25 19:32:39 +0100

    Webserver!

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -1,23 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/slack-go/slack"
 )
+
+func (s *slackGame) handleAction(token string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var i slack.InteractionCallback
+		s.logger.Info(r.FormValue("payload"))
+		err := json.Unmarshal([]byte(r.FormValue("payload")), &i)
+		if err != nil {
+			s.logger.Error(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		for _, action := range i.ActionCallback.BlockActions {
+			s.logger.Info(action.ActionID)
+
+			switch action.ActionID {
+			case "left":
+				s.game.Left()
+				s.updateGame()
+
+			case "right":
+				s.game.Right()
+				s.updateGame()
+
+			case "up":
+				s.game.Up()
+				s.updateGame()
+			case "down":
+				s.game.Down()
+				s.updateGame()
+			}
+		}
+	}
+}
 
 func (s *slackGame) msgOption() slack.MsgOption {
 	gameText := slack.NewTextBlockObject("plain_text", s.game.String(), true, false)
 	gameSection := slack.NewSectionBlock(gameText, nil, nil)
 
 	upBtnText := slack.NewTextBlockObject("plain_text", ":upvote:", true, false)
-	upBtn := slack.NewButtonBlockElement("", "click_me", upBtnText)
+	upBtn := slack.NewButtonBlockElement("up", "click_me", upBtnText)
 	leftBtnText := slack.NewTextBlockObject("plain_text", ":leftvote:", true, false)
-	leftBtn := slack.NewButtonBlockElement("", "click_me", leftBtnText)
+	leftBtn := slack.NewButtonBlockElement("left", "click_me", leftBtnText)
 	rightBtnText := slack.NewTextBlockObject("plain_text", ":rightvote:", true, false)
-	rightBtn := slack.NewButtonBlockElement("", "click_me", rightBtnText)
+	rightBtn := slack.NewButtonBlockElement("right", "click_me", rightBtnText)
 	downBtnText := slack.NewTextBlockObject("plain_text", ":downvote-red:", true, false)
-	downBtn := slack.NewButtonBlockElement("", "click_me", downBtnText)
+	downBtn := slack.NewButtonBlockElement("down", "click_me", downBtnText)
 
 	arrowBlock := slack.NewActionBlock("", leftBtn, downBtn, upBtn, rightBtn)
 
