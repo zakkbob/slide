104c096f9cfed108824248e1489f1480315aaef6
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-11-28 23:47:31 +0000

    Fix bug
    Should allow users to call the command without any args

--- pkg/bot.go
+++ pkg/bot.go
@@ -32,7 +32,6 @@ func (a *Application) HandleSlash() func(w http.ResponseWriter, r *http.Request)
 		}
 
 		args := strings.Split(strings.Trim(s.Text, " "), " ")
-
 		a.Logger.Error("Handling slash command", "command", s.Command, "text", s.Text, "args", args, "argCount", len(args))
 
 		var game Game
@@ -41,58 +40,49 @@ func (a *Application) HandleSlash() func(w http.ResponseWriter, r *http.Request)
 		case "/slide-test":
 			width, height := 3, 2
 
-			if len(args) != 0 {
-				switch args[0] {
-				case "default":
-					args = args[1:]
+			switch args[0] {
+			case "default", "":
+				args = args[1:]
 
-					if !(len(args) == 0 || len(args) == 2) {
-						a.Logger.Error("Received invalid number of command arguments", "command", s.Command, "args", s.Text, "count", len(args))
+				if !(len(args) == 0 || len(args) == 2) {
+					a.Logger.Error("Received invalid number of command arguments", "command", s.Command, "args", s.Text, "count", len(args))
+					w.WriteHeader(http.StatusBadRequest)
+					return
+				}
+
+				if len(args) == 2 {
+					width, err = strconv.Atoi(args[0])
+					if err != nil {
+						a.Logger.Error("Failed to parse command arguments", "command", s.Command, "args", s.Text)
 						w.WriteHeader(http.StatusBadRequest)
 						return
 					}
-
-					if len(args) == 2 {
-						width, err = strconv.Atoi(args[0])
-						if err != nil {
-							a.Logger.Error("Failed to parse command arguments", "command", s.Command, "args", s.Text)
-							w.WriteHeader(http.StatusBadRequest)
-							return
-						}
-						height, err = strconv.Atoi(args[1])
-						if err != nil {
-							a.Logger.Error("Failed to parse command arguments", "command", s.Command, "args", s.Text)
-							w.WriteHeader(http.StatusBadRequest)
-							return
-						}
-						if !(width > 0 && height > 0 && width*height <= 9) {
-							a.Logger.Error("Received invalid sliding puzzle dimensions", "width", width, "height", height)
-							w.WriteHeader(http.StatusBadRequest)
-							return
-						}
+					height, err = strconv.Atoi(args[1])
+					if err != nil {
+						a.Logger.Error("Failed to parse command arguments", "command", s.Command, "args", s.Text)
+						w.WriteHeader(http.StatusBadRequest)
+						return
 					}
-
-					solution := []string{
-						":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:",
+					if !(width > 0 && height > 0 && width*height <= 9) {
+						a.Logger.Error("Received invalid sliding puzzle dimensions", "width", width, "height", height)
+						w.WriteHeader(http.StatusBadRequest)
+						return
 					}
-					game = NewGame(solution, width, height, ":blank:")
-					game.DoRandomMoves(game.length * 10)
-
-				case "custom":
-					gameString := strings.Trim(s.Text[len(args[0])+1:], " ")
-					game = GameFromString(gameString)
-					game.gap = game.length - 1
-					game.gapVal = ":blank:"
-					game.DoRandomMoves(game.length * 10)
 				}
-			} else {
+
 				solution := []string{
 					":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:",
 				}
 				game = NewGame(solution, width, height, ":blank:")
 				game.DoRandomMoves(game.length * 10)
-			}
 
+			case "custom":
+				gameString := strings.Trim(s.Text[len(args[0])+1:], " ")
+				game = GameFromString(gameString)
+				game.gap = game.length - 1
+				game.gapVal = ":blank:"
+				game.DoRandomMoves(game.length * 10)
+			}
 		default:
 			a.Logger.Error("Received unknown command", "command", s.Command)
 			w.WriteHeader(http.StatusInternalServerError)
